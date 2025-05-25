a451ec94843a1ac48860dcba8d271aa7c5a3159d
onyenike <onyenikefestus2019@gmail.com>
2025-05-25 14:41:35 +0100

    Update 7-multi_c.js

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -1,10 +1,13 @@
+
 const count = parseInt(process.argv[2]);
 let output = '';
+
 if (isNaN(count) || count <= 0) {
   console.log("Missing number of occurrences");
 } else {
   for (let i = 0; i < count; i++) {
-    output += (i < count - 1) ? "c is fun\n" : "c is fun";
+    output += (i < count - 1) ? "C is fun\n" : "C is fun";
   }
   console.log(output);
 }
+
