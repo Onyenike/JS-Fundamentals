fa913f06293ab17feede597b9188059d215004e4
onyenike <onyenikefestus2019@gmail.com>
2025-05-25 14:35:50 +0100

    Update 10-factorial.js

--- 10-factorial.js
+++ 10-factorial.js
@@ -1,1 +1,11 @@
 
+function factorial(n) {
+  if (isNaN(n) || n <= 1) {
+    return 1;
+  }
+  return n * factorial(n - 1);
+}
+
+const num = parseInt(process.argv[2]);
+console.log(factorial(num));
+
